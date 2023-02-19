ab4767a14c551899774f575e0b8bb690195ac800
renuka wadi <revadarshan27@gmail.com>
2023-02-19 18:05:55 +0530

    final commit

--- Todo app/functions.py
+++ Todo app/functions.py
@@ -1,28 +1,43 @@
+""" backend functionality of PYsimpleGui buttons for gui.py  file
+    
+"""
+
 FILEPATH = r"D:\Python projects\Todo app\files\database.txt"
 
+# get_todos() :- return the lines(todos ) store in database file
 def get_todos(filepath=FILEPATH):
     with open(filepath,'r') as file:
             todos = file.readlines()
     return todos
 
+# write_todo() :- write into the database file
 def write_todos(todos, filepath=FILEPATH):
     with open(filepath,'w') as file:
             file.writelines(todos)
-            
+
+# adds a new todo into file            
 def add_todo(todo):
     todos = get_todos()
-    todos.append(todo + '/n')
+    todos.append(todo + '\n')
     write_todos(todos)
+    return todos
 
-
-def edit_todo(todo):
+# edits the existing todo
+def edit_todo(sel_todo,new_todo):
     todos = get_todos()
-    index = todos.index(todo)
-    return index
+    index = todos.index(sel_todo)
+    todos[index] = new_todo + '\n'
+    write_todos(todos)
+    return todos
 
+# delete todo from the file
 def delete_todo(todo):
     todos = get_todos()
     todos.pop(todos.index(todo))
     write_todos(todos)
+    return todos
 
-
+if __name__ == '__main__':
+    todos = get_todos()
+    print(todos.index('dance'))
+    print(todos)
--- Todo app/gui.py
+++ Todo app/gui.py
@@ -1,28 +1,48 @@
 import functions
 import PySimpleGUI as sg
 
+# initalize all the gui element
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip = "Enter todo",key= "todo")
 add_button = sg.Button("Add")
 edit_button = sg.Button("Edit")
 delete_button = sg.Button("Delete")
 exit_button = sg.Button("Exit")
+list_box = sg.Listbox(values = functions.get_todos(),
+                      key = 'sel_todo',
+                      enable_events = True,
+                      size = [45,10])
+
+# initiliaze the view of the window
 window = sg.Window('My To-Do App',layout = [[label],
                                             [input_box,add_button],
-                                            [edit_button, delete_button ],
+                                            [list_box ,edit_button, delete_button],
                                             [exit_button]])
 while True:
     event, value = window.read()
     match event:
         case 'Add':
             new_todo = value['todo']
-            functions.add_todo(new_todo)
-            print(functions.get_todos())
+            todos = functions.add_todo(new_todo)
+            window['sel_todo'].update(values = todos) # update the list view
+            window['todo'].update(value = '') #update the input_box
+            #print(functions.get_todos())
         case 'Edit':
-            new_todo = value['todo']
-            functions.edit_todo(new_todo)
+        
+            if value['todo'] != '':
+                todos = functions.edit_todo(sel_todo = value['sel_todo'][0],
+                               new_todo = value['todo'])
+                window['sel_todo'].update(values = todos) #refreshes the list view with new value
         case 'Delete':
-            functions.delete_todo(value['todo'])
+            todos = functions.delete_todo(value['todo'])
+            window['sel_todo'].update(values = todos)
+            window['todo'].update(value = '')
+            
+        case 'sel_todo':
+            window['todo'].update(value = value['sel_todo'][0]) # update input box on selection of a todo
         case 'Exit':
             break
+        case sg.WIN_CLOSED:
+            break
+    
 window.close()    
