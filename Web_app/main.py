be9b028a0811a0c2b52b43b88c6208f3d4af7ec4
Renuka-Wadikar <84169629+Renuka-Wadikar@users.noreply.github.com>
2023-02-22 11:26:57 +0530

    Add files via upload

---
+++ Web_app/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from PIL import Image
+
+#start the camera
+user_image = st.camera_input('Camera')
+
+if user_image:
+    #Create a pillow image
+    img = Image.open(user_image)
+    
+    #Convert image into grayscale
+    gray_img = img.convert('L')
+    
+    #render the grayscale image on local server
+    st.image(gray_img)
