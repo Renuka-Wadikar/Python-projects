8c3d0bd70e327256a729c54a3daba6ea966c55e6
renuka wadi <revadarshan27@gmail.com>
2023-02-22 13:15:09 +0530

    added instructions and images

--- Portfolio_app/main.py
+++ Portfolio_app/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
@@ -13,3 +14,21 @@ with col2:
     This website is showcase of various aaplication buil by me , while learning the language.
     Hope you enjoy the website!!!"""
     st.info(content)
+    
+col3,empty_col,col4 = st.columns([1.5,0.5,1.5])
+
+df = pandas.read_csv('data.csv', sep=';')
+
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source code]({row['url']})")
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row['title']) 
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source code]({row['url']})")
