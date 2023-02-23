31337332e8fdebac14ef3184056c645876d000bc
renuka wadi <revadarshan27@gmail.com>
2023-02-23 20:14:22 +0530

    added underlines to all the pages

--- PDF_generator/main.py
+++ PDF_generator/main.py
@@ -5,6 +5,7 @@ import os
 pdf = FPDF(orientation='P',unit='mm',format='A4')
 pdf.set_auto_page_break(auto=False, margin=0)
 
+
 df = pandas.read_csv("data.csv")
 
 
@@ -14,7 +15,12 @@ for index,row in df.iterrows():
     pdf.set_font(family='Times', style= 'B', size= 24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row['Topic'],align='L',ln=1)
-    pdf.line(10,21,200,21)
+    #underlined pages
+    for y in range(20,298,10):
+        pdf.line(10,y,200,y)
+        
+    
+    
     
     pdf.ln(265)
     
@@ -29,5 +35,8 @@ for index,row in df.iterrows():
         pdf.set_font(family='Times', style= 'I', size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0,h=8,txt= row['Topic'],align='R')
+        
+        for y in range(20,298,10):
+            pdf.line(10,y,200,y)
         
 pdf.output("output.pdf")
