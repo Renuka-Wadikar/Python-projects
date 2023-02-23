cae71e8783d6dcf6be548d793364ecc280d9b091
renuka wadi <revadarshan27@gmail.com>
2023-02-23 18:59:05 +0530

    initial commit

---
+++ PDF_generator/main.py
@@ -0,0 +1,18 @@
+import pandas 
+from fpdf import FPDF
+import os
+
+pdf = FPDF(orientation='P',unit='mm',format='A4')
+
+df = pandas.read_csv("data.csv")
+
+
+
+for index,row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family='Times', style= 'B', size= 24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, txt=row['Topic'],align='L',ln=1)
+    pdf.line(10,21,200,21)
+        
+pdf.output("output.pdf")
