97ce361f418b4e5d71893fd1c85138e172144830
renuka wadi <revadarshan27@gmail.com>
2023-02-23 10:33:01 +0530

    added  send email functionality to Contact Us Page

--- Portfolio_app/pages/Contact_Us.py
+++ Portfolio_app/pages/Contact_Us.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import send_email
 
 st.header('Contact Us')
+
+with st.form(key='email_form'):
+    user_email = st.text_input('Your email address')
+    message = st.text_area('Your message')
+
+    button = st.form_submit_button("Submit")
+    
+    if button:
+        send_email.sent_email(user_email,message)
+        st.info("Email was sent sucessfully")
---
+++ Portfolio_app/send_email.py
@@ -0,0 +1,22 @@
+import smtplib, ssl
+from email.message import EmailMessage
+import os
+
+
+def sent_email(user_email,message):
+    username  = 'wadikarrenuka@gmail.com'
+    msg = EmailMessage()
+    msg.set_content(message)
+    msg['Subject'] = 'A new email from ' + user_email
+    msg['From'] = username
+    msg['To'] = username
+    host = 'smtp.gmail.com'
+    port = 465
+
+    password = os.getenv('PASSWORD')
+    context  = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context= context) as server:
+        server.login(username, password)
+        server.send_message(msg)
+
