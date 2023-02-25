f63782cc57a2e5545dd38635d84301b8ca75202d
renuka wadi <revadarshan27@gmail.com>
2023-02-25 12:05:48 +0530

    added send email functionality

--- Email_news/main.py
+++ Email_news/main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 url = 'https://newsapi.org/v2/top-headlines?'\
         'sources=bbc-news&'\
@@ -10,5 +11,12 @@ request = requests.get(url)
 
 #create dictionary
 content = request.json()
+#creating the body of email
+
+email_msg = ''
 for article in content['articles']:
-    print(article['title'])
+    if article['title'] is not None:
+        email_msg = email_msg + article['title'] + '\n' + article['description'] + '\n'+ article['url'] + '\n\n'
+    
+send_email(email_msg)
+
---
+++ Email_news/send_email.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    message = "Subject: Today's news \n" + message
+    username = "renukawadikar27@gmail.com"
+    password = 'rgkhpzjljmmjdpjq' # genrate the app password from gmail account
+    
+    receiver = "renukawadikar27@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
