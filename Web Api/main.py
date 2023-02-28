7b0e920ce3d085aeb543d41ecbba97b103fa9beb
renuka wadi <revadarshan27@gmail.com>
2023-02-28 10:51:45 +0530

    added endpoints for yearly and station data

--- Web Api/main.py
+++ Web Api/main.py
@@ -12,7 +12,6 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-  
     filename = 'Web Api\data_small\TG_STAID' + str(station).zfill(6) + '.txt'
     df = pd.read_csv(filename, 
                      skiprows = 20,
@@ -23,6 +22,24 @@ def about(station, date):
         'station': station,
         'temprature': temprature
     }
+
+@app.route("/api/v1/<station>")
+def stationData(station):
+    filename = 'Web Api\data_small\TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, 
+                     skiprows = 20,
+                     parse_dates=["    DATE"] )
+    result = df.to_dict(orient='records')
+    return result
+
+@app.route("/api/v1/year/<station>/<year>")
+def oneYear(station, year):
+    filename = 'Web Api\data_small\TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, 
+                     skiprows = 20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))]
+    return result.to_dict(orient= 'records')
 
 if __name__ == "__main__":
     app.run(debug =True)
