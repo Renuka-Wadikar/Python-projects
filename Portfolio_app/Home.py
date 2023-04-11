a2f2141881895b29fd005be0894a81ca155a9c30
renuka wadi <revadarshan27@gmail.com>
2023-04-11 13:56:38 +0530

    changed summary and data file

--- Portfolio_app/Home.py
+++ Portfolio_app/Home.py
@@ -2,17 +2,24 @@ import streamlit as st
 import pandas
 
 st.set_page_config(layout='wide')
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1.5,5])
 
 with col1:
     st.image('images/photo.jpg')
 
 with col2:
     st.title("Renuka Wadikar")
-    content = """Hi, I am Renuka! I am a aspiring python programmer and python enthusiast.
-    I graduated in 2020 with BE in Computer Science.I love to learn things  by expermeting with the code.
-    This website is showcase of various application build by me , while learning the language.
-    Hope you enjoy the website!!!"""
+    content = """Hi, I am Renuka! As a recent graduate in Computer Science
+    with a keen interest in programming,I have developed a strong passion for 
+    the world of Python programming. With a solid foundation in the principles
+    of software engineering and a deep understanding of programming concepts,
+    I am confident in my ability to create complex and efficient software solutions.
+    
+    My experience includes hands-on work with Python programming, including building
+    web applications, data analysis and visualization, and machine learning.
+    I have also worked on various projects, from simple scripts to complex applications.
+    
+    Through this site, I aim to share all the projects ,I created while learning the language"""
     st.info(content)
     
 col3,empty_col,col4 = st.columns([1.5,0.5,1.5])
